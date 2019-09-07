38aae1a44cf98f2dbefe90e264f4ab3a4646ee68
Dolores <alyciahathaways@gmail.com>
2019-09-07 21:11:12 +0800

    重构 login 组件的 form 表单

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,19 +5,14 @@
 				<div class="modal-container">
 					<div class="main"></div>
 					<div class="form">
-						<h3 @click="showRegister">创建账户</h3>
-                        <div :class="{show: isShowRegister}" class="register">
-                            <input type="text" @keyup.enter="onRegister" v-model="register.username" placeholder="请输入用户名" />
-                            <input type="password" @keyup.enter="onRegister" v-model="register.password" placeholder="密码" />
-                            <p :class="{error: register.isError}"> {{register.notice}}</p>
-                            <div class="button" @click="onRegister">创建账号</div>
-                        </div>
-						<h3 @click="showLogin">登录</h3>
-                        <div :class="{show: isShowLogin}" class="login">
-                            <input type="text" @keyup.enter="onLogin" v-model="login.username" placeholder="请输入用户名" />
-                            <input type="password" @keyup.enter="onLogin" v-model="login.password" placeholder="密码" />
-                            <p :class="{error: login.isError}"> {{login.notice}}</p>
-                            <div class="button" @click="onLogin">登录</div>
+                        <h3 @click="toggleForm">创建账户</h3>
+						<h3 :class="{show: !isShowLogin}" @click="toggleForm">登录</h3>
+                        <div :class="{show: !isShowLogin}" class="login">
+                            <input type="text" @keyup.enter="submit" v-model="form.username" placeholder="请输入用户名" />
+                            <input type="password" @keyup.enter="submit" v-model="form.password" placeholder="密码" />
+                            <p :class="{error: form.isError}">{{form.notice}}</p>
+                            <div class="button" v-if="!isShowLogin" @click="submit">创建账号</div>
+                            <div class="button" v-if="isShowLogin" @click="submit">登录</div>
                         </div>
 					</div>
 				</div>
@@ -40,79 +35,55 @@ export default {
 	data() {
 		return {
 			isShowLogin: true,
-			isShowRegister: false,
-			login: {
+			form: {
 				username: '',
 				password: '',
-				notice: '输入用户名和密码',
-				isError: false
-			},
-			register: {
-				username: '',
-				password: '',
-				notice: '创建账号后，请记住用户名和密码',
+				notice: '请输入用户名和密码',
 				isError: false
 			}
 		}
 	},
 	methods: {
-		showLogin() {
-			this.isShowLogin = true
-			this.isShowRegister = false
-		},
-		showRegister() {
-			this.isShowLogin = false
-			this.isShowRegister = true
-		},
-		onRegister() {
-			if (!/^[\w\u4e00-\u9fa5]{3,15}$/.test(this.register.username)) {
-				this.register.isError = true
-				this.register.notice =
-					'用户名3~15个字符，仅限于字母数字下划线中文'
+        toggleForm() {
+            this.isShowLogin = !this.isShowLogin
+            this.form = {
+                username: '',
+				password: '',
+				notice: '请输入用户名和密码',
+				isError: false
+            }
+        },
+        getAPI() {
+            let data = {
+                username: this.form.username,
+                password: this.form.password
+            }
+            if (this.isShowLogin) {
+                return Auth.login(data)
+            }else {
+                return Auth.register(data)
+            }
+        },
+		submit() {
+			if (!/^[\w\u4e00-\u9fa5]{3,15}$/.test(this.form.username)) {
+				this.form.isError = true
+				this.form.notice = '用户名3~15个字符，仅限于字母数字下划线中文'
 				return
 			}
-			if (!/^.{6,16}$/.test(this.register.password)) {
-				this.register.isError = true
-				this.register.notice = '密码长度为6~16个字符'
+			if (!/^.{6,16}$/.test(this.form.password)) {
+				this.form.isError = true
+				this.form.notice = '密码长度为6~16个字符'
 				return
 			}
-
-            Auth.register({
-                username: this.register.username,
-                password: this.register.password
-            }).then(data => {
-                this.register.isError = false
-                this.register.notice = ''
-                Bus.$emit('userInfo', {username: this.login.username})
+            
+            this.getAPI().then(data => {
+                this.form.isError = false
+                this.form.notice = ''
+                Bus.$emit('userInfo', {username: this.form.username})
                 this.$router.push({path: 'notebook'})
             }).catch(error => {
-                this.register.isError = true
-			    this.register.notice = error.msg
-            })
-		},
-		onLogin() {
-			if (!/^[\w\u4e00-\u9fa5]{3,15}$/.test(this.login.username)) {
-				this.login.isError = true
-				this.login.notice = '用户名3~15个字符，仅限于字母数字下划线中文'
-				return
-			}
-			if (!/^.{6,16}$/.test(this.login.password)) {
-				this.login.isError = true
-				this.login.notice = '密码长度为6~16个字符'
-				return
-			}
-			
-            Auth.login({
-                username: this.login.username,
-                password: this.login.password
-            }).then(data => {
-                this.login.isError = false
-                this.login.notice = ''
-                Bus.$emit('userInfo', {username: this.login.username})
-                this.$router.push({path: 'notebook'})
-            }).catch(error => {
-                this.login.isError = true
-			    this.login.notice = error.msg
+                this.form.isError = true
+			    this.form.notice = error.msg
             })
 		}
 	}
@@ -154,43 +125,35 @@ export default {
 		background-size: contain;
 	}
 	.form {
+        position: relative;
 		width: 270px;
 		border-left: 1px solid #ccc;
         overflow: hidden;
 		h3 {
+            width: 100%;
 			padding: 10px 20px;
             margin-top: -1px;
 			font-weight: normal;
 			font-size: 16px;
 			border-top: 1px solid #eee;
 			cursor: pointer;
-
+            transition: all .3s;
 			&:nth-of-type(2) {
 				border-bottom: 1px solid #eee;
 			}
+            &.show {
+                transform: translateY(193px);
+            }
 		}
 
-		.button {
-			background-color: #2bb964;
-			height: 36px;
-			line-height: 36px;
-			text-align: center;
-			font-weight: bold;
-			color: #fff;
-			border-radius: 4px;
-			margin-top: 18px;
-			cursor: pointer;
-		}
-
-		.login,
-		.register {
+		.login {
 			padding: 0 20px;
 			border-top: 1px solid #eee;
-            height: 0;
+            height: 193px;
             overflow: hidden;
-            transition: height .3s;
+            transition: all .5s;
             &.show {
-                height: 193px;
+                transform: translateY(-43px);
             }
 			input {
 				display: block;
@@ -216,9 +179,17 @@ export default {
 			.error {
 				color: red;
 			}
-		}
-		.login {
-			border-top: 0;
+            .button {
+                background-color: #2bb964;
+                height: 36px;
+                line-height: 36px;
+                text-align: center;
+                font-weight: bold;
+                color: #fff;
+                border-radius: 4px;
+                margin-top: 18px;
+                cursor: pointer;
+            }
 		}
 	}
 }

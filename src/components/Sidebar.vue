4f7e7d4714c262a53e2c8b8c66f52d566ec1c717
Dolores <alyciahathaways@gmail.com>
2019-09-07 10:16:40 +0800

    更新页面跳转逻辑

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -32,7 +32,7 @@ export default {
         logout() {
             Auth.logout()
                 .then(data => {
-                    console.log(data)
+                    this.$router.push({path: 'login'})
                 })
         }
     }

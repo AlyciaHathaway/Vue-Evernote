0a5bba1703a5dbd5d220614ada07dd3d8dba1133
Dolores <alyciahathaways@gmail.com>
2019-09-15 00:16:19 +0800

    完成笔记的保存功能

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -2,36 +2,47 @@
 	<div class="detail" id="notebook">
 		<note-sidebar @update:notes="value => noteList = value"></note-sidebar>
 		<div class="note-detail">
-            <div class="note-empty" v-show="!currentNote">
-                请选择笔记
-            </div>
-            <template v-if="currentNote && currentNote.id">
-                <div class="note-bar">
-                    <p>创建日期: {{currentNote.createdAtDate}}</p>
-                    <p>更新日期: {{currentNote.updatedAtDate}}</p>
-                    <p class="save">{{currentNote.statusText}}</p>
-                    <div class="operate">
-                        <g-icon class="icon" name="trash"></g-icon>
-                        <g-icon class="icon" name="fullscreen"></g-icon>
-                    </div>
-                </div>
-                <div class="note-title">
-                    <input type="text" v-model="currentNote.title" placeholder="请输入标题">
-                </div>
-                <div class="editor">
-                    <textarea v-show="true" :value="currentNote.content" placeholder="内容区域，支持 Markdown 语法"></textarea>
-                    <div class="preview markdown-body" v-show="false"></div>
-                </div>
-            </template>
-			
+			<div class="note-empty" v-show="!currentNote">请选择笔记</div>
+			<template v-if="currentNote && currentNote.id">
+				<div class="note-bar">
+					<p>创建日期: {{currentNote.createdAtDate}}</p>
+					<p>更新日期: {{currentNote.updatedAtDate}}</p>
+					<p class="save">{{editStatus}}</p>
+					<div class="operate">
+						<g-icon class="icon" name="trash"></g-icon>
+						<g-icon class="icon" name="fullscreen"></g-icon>
+					</div>
+				</div>
+				<div class="note-title">
+					<input
+						type="text"
+						v-model="currentNote.title"
+						@input="editingNote"
+						@keydown="editStatus='正在输入...'"
+						placeholder="请输入标题"
+					/>
+				</div>
+				<div class="editor">
+					<textarea
+						v-show="true"
+						v-model="currentNote.content"
+						@input="editingNote"
+						@keydown="editStatus='正在输入...'"
+						placeholder="内容区域，支持 Markdown 语法"
+					></textarea>
+					<div class="preview markdown-body" v-show="false"></div>
+				</div>
+			</template>
 		</div>
 	</div>
 </template>
 
 <script>
 import NoteSidebar from '@/components/NoteSidebar'
+import Note from '@/apis/notedetail'
 import Icon from '@/components/Icon'
 import Bus from '@/helpers/eventBus'
+import _ from 'lodash'
 
 export default {
 	name: 'Notebook',
@@ -41,19 +52,41 @@ export default {
 	},
 	data() {
 		return {
-            currentNote: {},
-            noteList: []
+			currentNote: {},
+			noteList: [],
+			editStatus: '笔记未改动'
 		}
 	},
-    created() {
-        Bus.$once('update:notes', value => {
-            this.currentNote = value.find(note => note.id === this.$route.query.noteID || {})
-        })
-    },
-    beforeRouteUpdate(to, from, next) {
-        this.currentNote = this.noteList.find(note => note.id === parseInt(to.query.noteID))
-        next()
-    }
+	created() {
+		Bus.$once('update:notes', value => {
+			this.currentNote = value.find(
+				note => note.id === this.$route.query.noteID || {}
+			)
+		})
+	},
+	beforeRouteUpdate(to, from, next) {
+		this.currentNote = this.noteList.find(
+			note => note.id === parseInt(to.query.noteID)
+		)
+		next()
+	},
+	methods: {
+		editingNote: _.debounce(function() {
+			Note.updateNote(
+				{ noteID: this.currentNote.id },
+				{
+					title: this.currentNote.title,
+					content: this.currentNote.content
+				}
+			)
+				.then(data => {
+					this.editStatus = '已保存'
+				})
+				.catch(error => {
+					this.editStatus = '保存失败'
+				})
+		}, 500)
+	}
 }
 </script>
 
@@ -78,7 +111,7 @@ export default {
 		}
 		.note-bar {
 			display: flex;
-            align-items: center;
+			align-items: center;
 			padding: 4px 20px;
 			border-bottom: 1px solid #eee;
 			p {
@@ -86,19 +119,19 @@ export default {
 				color: #999;
 				margin-right: 6px;
 			}
-            .operate {
-                margin-left: auto;
-                display: flex;
-                align-items: center;
-                .icon {
-                    &:last-child {
-                        margin-right: 20px;
-                    }
-                    margin-right: 10px;
-                    font-size: 8px;
-                    cursor: pointer;
-                }
-            }
+			.operate {
+				margin-left: auto;
+				display: flex;
+				align-items: center;
+				.icon {
+					&:last-child {
+						margin-right: 20px;
+					}
+					margin-right: 10px;
+					font-size: 8px;
+					cursor: pointer;
+				}
+			}
 			.save {
 				margin-left: 4px;
 				flex: 1;
